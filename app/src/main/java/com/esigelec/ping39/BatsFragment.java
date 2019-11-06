0c411ce869651a175e75535d8f304d3b9d1854cf
lalamefine <l.triboulin@gmail.com>
2019-11-06 14:23:01 +0100

    Original

---
+++ app/src/main/java/com/esigelec/ping39/BatsFragment.java
@@ -0,0 +1,75 @@
+package com.esigelec.ping39;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link BatsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link BatsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BatsFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public BatsFragment() {
+        // Required empty public constructor
+    }
+
+    public static BatsFragment newInstance() {
+        BatsFragment fragment = new BatsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bats, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/esigelec/ping39/MainActivity.java
@@ -0,0 +1,115 @@
+package com.esigelec.ping39;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        RoulisFragment.OnFragmentInteractionListener, BatsFragment.OnFragmentInteractionListener, CapFragment.OnFragmentInteractionListener {
+    DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        fragmentRoulis = RoulisFragment.newInstance();
+        this.startTransactionFragment(fragmentRoulis);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //FOR FRAGMENTS
+    // 1 - Declare fragment handled by Navigation Drawer
+    private Fragment fragmentBats;
+    private Fragment fragmentCap;
+    private Fragment fragmentRoulis;
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        // 6 - Show fragment after user clicked on a menu item
+        switch (id){
+            case R.id.nav_bats :
+                if (fragmentBats == null) fragmentBats = BatsFragment.newInstance();
+                this.startTransactionFragment(fragmentBats);
+                break;
+            case R.id.nav_cap:
+                if (fragmentCap == null) fragmentCap = CapFragment.newInstance();
+                this.startTransactionFragment(fragmentCap);
+                break;
+            case R.id.nav_roulis:
+                if (fragmentRoulis == null) fragmentRoulis = RoulisFragment.newInstance();
+                this.startTransactionFragment(fragmentRoulis);
+                break;
+            default:
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    // 3 - Generic method that will replace and show a fragment inside the MainActivity Frame Layout
+    private void startTransactionFragment(Fragment fragment){
+        if (!fragment.isVisible()){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_holder, fragment).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
