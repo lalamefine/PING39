e16765273aeaeddc8949f2371193c386e8e598f8
lalamefine <l.triboulin@gmail.com>
2019-11-14 12:52:56 +0100

    Sensors configuration

--- app/src/main/java/com/esigelec/ping39/RoulisFragment.java
+++ app/src/main/java/com/esigelec/ping39/RoulisFragment.java
@@ -2,10 +2,13 @@ package com.esigelec.ping39;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,12 +28,36 @@ public class RoulisFragment extends Fragment {
     private SensorManager sensorManager;
     private Sensor sensorGrav;
     private Sensor sensorGyro;
+    private float[] gravValues;
+    //private float[] gyroValues;
 
-    public RoulisFragment() {
-        // Required empty public constructor
+    //EVENEMENTS SUR L'ACCELEROMETTRE
+    final SensorEventListener gravEventListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            gravValues = sensorEvent.values;
+            Log.d("Sensors", "Gravité (z,x,y) : " + gravValues[0] + "," + gravValues[1] + "," + gravValues[2]);
+        }
+    };
+    //EVENEMENTS SUR LE GYROSCOPE
+    //final SensorEventListener gyroEventListener = new SensorEventListener() {
+    //    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    //    public void onSensorChanged(SensorEvent sensorEvent) {
+    //        gyroValues = sensorEvent.values;
+    //        Log.d("Sensors", "Rotation sur l'axe z : " + gyroValues[0]);
+    //        Log.d("Sensors", "Rotation sur l'axe x : " + gyroValues[1]);
+    //        Log.d("Sensors", "Rotation sur l'axe y : " + gyroValues[2]);
+    //    }
+    //};
+
+    public RoulisFragment() {}
+
+    private void refreshView(){
+        float[] values = new float[3];
+        float[] R = new float[9];
+
     }
 
-    // TODO: Rename and change types and number of parameters
     public static RoulisFragment newInstance() {
         RoulisFragment fragment = new RoulisFragment();
         Bundle args = new Bundle();
@@ -41,11 +68,13 @@ public class RoulisFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sensorGrav = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        onAttach(this.getContext());
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_roulis, container, false);
     }
@@ -63,8 +92,8 @@ public class RoulisFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
             sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
-            sensorGrav = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-            sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            sensorManager.registerListener(gravEventListener, sensorGrav, SensorManager.SENSOR_DELAY_NORMAL);
+            //sensorManager.registerListener(gyroEventListener, sensorGyro, SensorManager.SENSOR_DELAY_NORMAL);
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -74,6 +103,7 @@ public class RoulisFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+        sensorManager.unregisterListener(gravEventListener, sensorGrav);
         mListener = null;
     }
 
